39352c8239517f16bc1040ecc0e4f46a7f3f1224
ChileMathAcc <chilese@outlook.com>
2024-10-07 14:05:47 +0100

    Adds code to print out a random word from the list of friuts and take in a users guess

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,2 +1,10 @@
+import random
+
 word_list = ["Guava", "Apple", "Peach", "Blueberry", "Pear"]
 print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input('Input a single letter: ')
+
