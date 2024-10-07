e692be9bc264b563c0424392c501ed038941a3ce
ChileMathAcc <chilese@outlook.com>
2024-10-07 14:11:55 +0100

    Adds code to check if the guessed letter is valid

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -7,4 +7,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input('Input a single letter: ')
-
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess')
+else:
+    print('Oops! That is not a valid input.')
+    
